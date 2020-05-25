e937d75d962cf87c52969df740317c9bf3205155
liaqat <liaqatalihaidari35@gmail.com>
2020-05-25 23:48:39 +0530

    init commit

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const express = require('express');
+
+const hbs = require('hbs');
+
+const app = express();
+
+// setting view engine
+app.set('view engine','hbs');
+
+// setting views path
+const viewsPath = path.join(__dirname,'../templates/views/');
+app.set('views',viewsPath);
+
+// setting partial path
+const partialsPath = path.join(__dirname,'../templates/partials/');
+hbs.registerPartials(partialsPath);
+
+// setting public dir path
+const public_dir_path = path.join(__dirname,'../public');
+app.use(express.static(public_dir_path));
+
+app.get('',(req,res)=>{
+  res.render('index',{
+    title:'Home',
+    heading:'This is the heading'
+  });
+});
+
+app.get('/about',(req,res)=>{
+  res.render('about');
+});
+app.get('/help',(req,res)=>{
+  res.render('help');
+});
+
+app.listen(3000,()=>{
+  console.log('server is running');
+});
